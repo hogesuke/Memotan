08ca2189a99bef9e4ee7d3a20d871c9ccf769832
hogesuke <miyado@gmail.com>
2013-09-09 05:01:05 +0900

    セッションの有効期限を1ヶ月に設定

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-ToriatamaAlpha::Application.config.session_store :cookie_store, :key => '_Toriatama_alpha_session'
+ToriatamaAlpha::Application.config.session_store :cookie_store,
+  :key => '_Toriatama_alpha_session',
+  :expire_after => 1.months
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
