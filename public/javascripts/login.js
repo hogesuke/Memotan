7c35eb479b67e19424766e035145d80023a9aff0
hogesuke <miyado@gmail.com>
2013-10-23 06:58:54 +0900

    ソーシャルアイコンをログイン画面に追加

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,3 +1,22 @@
-$('.login-button').live('click', function() {
-    $(this).closest('form').submit();
+$(function() {
+    $('.login-button').live('click', function() {
+        $(this).closest('form').submit();
+    });
+
+    $("#socialbuttons .twitter").socialbutton("twitter", {
+        button : "horizontal",
+        text   : "単語をメモするアプリ「Memotan」",
+        url    : "http://www.memotan.com",
+    }).width(95);
+
+    $("#socialbuttons .facebook").socialbutton("facebook_like", {
+        button : "button_count",
+        url    : "http://www.memotan.com",
+    }).width(110);
+
+    $("#socialbuttons .hatena").socialbutton("hatena", {
+        button : "standard",
+        url    : "http://www.memotan.com",
+        title  : "Memotan",
+    }).width(70);
 });
